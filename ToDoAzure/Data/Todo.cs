7a41316266b565788ff24375912e8532c1627e13
Radosław Kamiński <radoslaw.kaminski96@gmail.com>
2020-02-05 11:39:21 +0100

    Added todo model and repository

---
+++ ToDoAzure/Data/Todo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoAzure.Data
+{
+    public class Todo
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public bool Completed { get; set; }
+
+        protected Todo()
+        {
+
+        }
+
+        public Todo(string title)
+        {
+            Id = new Guid();
+            Title = title;
+        }
+    }
+}
