1af4c4e6d7fd7dee288e35fa485e8f36807f47b2
Radosław Kamiński <radoslaw.kaminski96@gmail.com>
2020-02-05 14:16:03 +0100

    Added Active and Completed tab

---
+++ ToDoAzure/ActiveTodos.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace ToDoAzure
+{
+    partial class ActiveTodos
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ToDo App | Active";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.SteelBlue;
+            this.panel1.Location = new System.Drawing.Point(4, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(11, 393);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(21, 24);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(626, 393);
+            this.panel2.TabIndex = 0;
+            // 
+            // ActiveTodos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Name = "ActiveTodos";
+            this.Size = new System.Drawing.Size(650, 420);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
--- ToDoAzure/ToDoForm.Designer.cs
+++ ToDoAzure/ToDoForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.closeButton = new System.Windows.Forms.PictureBox();
             this.gradientPanel1 = new ToDoAzure.Design.GradientPanel();
             this.panel2 = new System.Windows.Forms.Panel();
             this.label2 = new System.Windows.Forms.Label();
@@ -35,14 +36,26 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.closeButton = new System.Windows.Forms.PictureBox();
+            this.completedTodos1 = new ToDoAzure.CompletedTodos();
+            this.activeTodos1 = new ToDoAzure.ActiveTodos();
+            ((System.ComponentModel.ISupportInitialize)(this.closeButton)).BeginInit();
             this.gradientPanel1.SuspendLayout();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.closeButton)).BeginInit();
             this.SuspendLayout();
+            // 
+            // closeButton
+            // 
+            this.closeButton.Image = global::ToDoAzure.Properties.Resources.close;
+            this.closeButton.Location = new System.Drawing.Point(830, 0);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(30, 30);
+            this.closeButton.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.closeButton.TabIndex = 1;
+            this.closeButton.TabStop = false;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
             // 
             // gradientPanel1
             // 
@@ -53,7 +66,7 @@
             this.gradientPanel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.gradientPanel1.Location = new System.Drawing.Point(0, 0);
             this.gradientPanel1.Name = "gradientPanel1";
-            this.gradientPanel1.Size = new System.Drawing.Size(167, 450);
+            this.gradientPanel1.Size = new System.Drawing.Size(167, 570);
             this.gradientPanel1.TabIndex = 0;
             // 
             // panel2
@@ -61,16 +74,16 @@
             this.panel2.BackColor = System.Drawing.Color.Transparent;
             this.panel2.Controls.Add(this.label2);
             this.panel2.Controls.Add(this.pictureBox2);
-            this.panel2.Location = new System.Drawing.Point(0, 340);
+            this.panel2.Location = new System.Drawing.Point(0, 450);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(167, 110);
+            this.panel2.Size = new System.Drawing.Size(167, 120);
             this.panel2.TabIndex = 2;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label2.Location = new System.Drawing.Point(51, 19);
+            this.label2.Location = new System.Drawing.Point(51, 37);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(79, 20);
             this.label2.TabIndex = 4;
@@ -79,7 +92,7 @@
             // pictureBox2
             // 
             this.pictureBox2.Image = global::ToDoAzure.Properties.Resources.microphone;
-            this.pictureBox2.Location = new System.Drawing.Point(65, 51);
+            this.pictureBox2.Location = new System.Drawing.Point(64, 69);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(45, 39);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -93,7 +106,7 @@
             this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(165, 143);
+            this.panel1.Size = new System.Drawing.Size(165, 152);
             this.panel1.TabIndex = 0;
             // 
             // label1
@@ -116,22 +129,27 @@
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
-            // closeButton
+            // completedTodos1
             // 
-            this.closeButton.Image = global::ToDoAzure.Properties.Resources.close;
-            this.closeButton.Location = new System.Drawing.Point(739, 0);
-            this.closeButton.Name = "closeButton";
-            this.closeButton.Size = new System.Drawing.Size(30, 30);
-            this.closeButton.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.closeButton.TabIndex = 1;
-            this.closeButton.TabStop = false;
-            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            this.completedTodos1.Location = new System.Drawing.Point(171, 150);
+            this.completedTodos1.Name = "completedTodos1";
+            this.completedTodos1.Size = new System.Drawing.Size(650, 420);
+            this.completedTodos1.TabIndex = 2;
+            // 
+            // activeTodos1
+            // 
+            this.activeTodos1.Location = new System.Drawing.Point(173, 150);
+            this.activeTodos1.Name = "activeTodos1";
+            this.activeTodos1.Size = new System.Drawing.Size(650, 420);
+            this.activeTodos1.TabIndex = 3;
             // 
             // ToDoForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(769, 450);
+            this.ClientSize = new System.Drawing.Size(860, 570);
+            this.Controls.Add(this.activeTodos1);
+            this.Controls.Add(this.completedTodos1);
             this.Controls.Add(this.closeButton);
             this.Controls.Add(this.gradientPanel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -140,6 +158,7 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "ToDoForm";
             this.Load += new System.EventHandler(this.ToDoForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.closeButton)).EndInit();
             this.gradientPanel1.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
@@ -147,7 +166,6 @@
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.closeButton)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -162,5 +180,7 @@
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.PictureBox closeButton;
+        private CompletedTodos completedTodos1;
+        private ActiveTodos activeTodos1;
     }
 }
