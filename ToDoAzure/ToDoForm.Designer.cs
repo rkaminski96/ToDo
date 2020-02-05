2b963ae8495727ee2c4c0c9069a0b69f6dd80f50
Radosław Kamiński <radoslaw.kaminski96@gmail.com>
2020-02-05 12:42:35 +0100

    Added gradient panel

--- ToDoAzure/ToDoForm.Designer.cs
+++ ToDoAzure/ToDoForm.Designer.cs
@@ -28,12 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.gradientPanel1 = new ToDoAzure.Design.GradientPanel();
+            this.SuspendLayout();
+            // 
+            // gradientPanel1
+            // 
+            this.gradientPanel1.ColorBottom = System.Drawing.Color.SteelBlue;
+            this.gradientPanel1.ColorTop = System.Drawing.Color.DarkViolet;
+            this.gradientPanel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.gradientPanel1.Location = new System.Drawing.Point(0, 0);
+            this.gradientPanel1.Name = "gradientPanel1";
+            this.gradientPanel1.Size = new System.Drawing.Size(167, 450);
+            this.gradientPanel1.TabIndex = 0;
+            // 
+            // ToDoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(769, 450);
+            this.Controls.Add(this.gradientPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ToDoForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "ToDoForm";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Design.GradientPanel gradientPanel1;
     }
 }
