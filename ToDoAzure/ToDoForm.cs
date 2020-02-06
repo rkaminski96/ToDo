b40cf06f52442f149ce4c40635af2cbffdac0218
Radosław Kamiński <radoslaw.kaminski96@gmail.com>
2020-02-06 15:16:37 +0100

    Update todo works

--- ToDoAzure/ToDoForm.cs
+++ ToDoAzure/ToDoForm.cs
@@ -21,7 +21,8 @@ namespace ToDoAzure
             InitializeComponent();
         }
 
-        private string CheckToDoStatus(Todo todo)
+
+        private string checkToDoStatus(Todo todo)
         {
             var toDoStatus = "";
             if (todo.Completed == false)
@@ -29,6 +30,13 @@ namespace ToDoAzure
             else
                 toDoStatus = "COMPLETED";
             return toDoStatus;
+        }
+
+        private string trimResult(string result)
+        {
+            char[] trimChars = {' ', '.', ','};
+            result = result.TrimEnd(trimChars);
+            return result;
         }
 
         private void closeButton_Click(object sender, EventArgs e)
@@ -40,24 +48,47 @@ namespace ToDoAzure
         {
             SpeechService speechService = new SpeechService();
             var result = await speechService.RecognizeSpeechAsync();
+            result = trimResult(result);
 
             var todo = new Todo(result);
             todos.Add(todo);
 
             ListViewItem item = new ListViewItem(todo.Title);
 
-            var toDoStatus = CheckToDoStatus(todo);
+            var toDoStatus = checkToDoStatus(todo);
             item.SubItems.Add(toDoStatus);
             listViewAll.Items.Add(item);
         }
 
-        private void markAsDoneButton_Click(object sender, EventArgs e)
+        private async void markAsDoneButton_Click(object sender, EventArgs e)
         {
+            SpeechService speechService = new SpeechService();
+            var result = await speechService.RecognizeSpeechAsync();
+            result = trimResult(result);
 
+            var completedTodo = todos.FirstOrDefault(s => s.Title == result);
+            if (completedTodo == null)
+            {
+                MessageBox.Show("Todo with this name doesn't exist");
+            }
+            else
+            {
+                completedTodo.Completed = true;
+            }
         }
 
         private void allTodosButton_Click(object sender, EventArgs e)
         {
+            listViewAll.Items.Clear();
+
+            foreach (var todo in todos)
+            {
+                ListViewItem item = new ListViewItem(todo.Title);
+                var toDoStatus = checkToDoStatus(todo);
+                item.SubItems.Add(toDoStatus);
+                listViewAll.Items.Add(item);
+            }
+
             listViewAll.BringToFront();
             toDoStatusLabel.Text = "Todo App | All";
         }
@@ -69,17 +100,28 @@ namespace ToDoAzure
             foreach (var todo in todos)
             {
                 ListViewItem item = new ListViewItem(todo.Title);
-                var toDoStatus = CheckToDoStatus(todo);
+                var toDoStatus = checkToDoStatus(todo);
                 item.SubItems.Add(toDoStatus);
                 listViewActive.Items.Add(item);
             }
 
             listViewActive.BringToFront();
             toDoStatusLabel.Text = "Todo App | Active";
+            
         }
 
         private void completedTodosButton_Click(object sender, EventArgs e)
         {
+            listViewCompleted.Items.Clear();
+
+            foreach (var todo in todos)
+            {
+                ListViewItem item = new ListViewItem(todo.Title);
+                var toDoStatus = checkToDoStatus(todo);
+                item.SubItems.Add(toDoStatus);
+                listViewCompleted.Items.Add(item);
+            }
+
             listViewCompleted.BringToFront();
             toDoStatusLabel.Text = "Todo App | Completed";
         }
